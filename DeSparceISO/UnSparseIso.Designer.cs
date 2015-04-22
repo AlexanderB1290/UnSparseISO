c0c1846e9f316ad2b0bec3830182829cfc60680b
Aleksandar Bushev <alexanderb@rila.bg>
2015-04-22 15:47:42 +0300

    application for removing sparse flag from ISO files or any other files

---
+++ DeSparceISO/UnSparseIso.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DeSparceISO
+{
+    partial class UnSparseIso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.prgExecution = new System.Windows.Forms.ProgressBar();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.getFileNameLocation = new System.Windows.Forms.OpenFileDialog();
+            this.btnUnSparse = new System.Windows.Forms.Button();
+            this.toolTipRemoveSparceFlag = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // prgExecution
+            // 
+            this.prgExecution.Location = new System.Drawing.Point(2, 58);
+            this.prgExecution.Maximum = 50;
+            this.prgExecution.Name = "prgExecution";
+            this.prgExecution.Size = new System.Drawing.Size(280, 10);
+            this.prgExecution.TabIndex = 0;
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(2, 3);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(199, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(207, 1);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // getFileNameLocation
+            // 
+            this.getFileNameLocation.Filter = "ISO|*.iso|All files|*.*";
+            this.getFileNameLocation.InitialDirectory = "./";
+            // 
+            // btnUnSparse
+            // 
+            this.btnUnSparse.Location = new System.Drawing.Point(2, 29);
+            this.btnUnSparse.Name = "btnUnSparse";
+            this.btnUnSparse.Size = new System.Drawing.Size(280, 23);
+            this.btnUnSparse.TabIndex = 3;
+            this.btnUnSparse.Text = "Remove Sparse Flag";
+            this.btnUnSparse.UseVisualStyleBackColor = true;
+            this.btnUnSparse.Click += new System.EventHandler(this.btnDeSparce_Click);
+            // 
+            // UnSparseIso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.Controls.Add(this.btnUnSparse);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.prgExecution);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "UnSparseIso";
+            this.Text = "Remove Sparce Flag";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.ProgressBar prgExecution;
+        private System.Windows.Forms.OpenFileDialog getFileNameLocation;
+        private System.Windows.Forms.Button btnUnSparse;
+        private System.Windows.Forms.ToolTip toolTipRemoveSparceFlag;
+    }
+}
+
---
+++ DeSparceISO/UnSparseIso.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Permissions;
+using System.Diagnostics;
+
+
+namespace DeSparceISO
+{
+    public partial class UnSparseIso : Form
+    {
+        private String file_location = "";
+        private String[] path_segments = {};
+        private String current_dir = "";
+        public UnSparseIso()
+        {
+            InitializeComponent();
+
+            this.current_dir = Directory.GetCurrentDirectory();
+            this.toolTipRemoveSparceFlag.SetToolTip(txtFileName, "Write the filename with extension in the current directory.");
+            this.toolTipRemoveSparceFlag.SetToolTip(btnBrowse, "Browse to the location of the ISO file and select it.");
+            this.toolTipRemoveSparceFlag.SetToolTip(btnUnSparse, "Remove the sparce flag from the selected file.");
+            this.toolTipRemoveSparceFlag.SetToolTip(prgExecution, "Show execution progress of the current operation.");
+
+            FileIOPermission file_permissions = new FileIOPermission(PermissionState.Unrestricted);
+            file_permissions.AllLocalFiles = FileIOPermissionAccess.AllAccess;
+            try
+            {
+                file_permissions.Demand();
+            }
+            catch (Exception exp) {
+                showError(exp.Message);
+            }
+        }
+
+        private void showError(String message){
+            MessageBox.Show(message, "There was a problem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void showInfo(String message) {
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private static FileAttributes removeAttribute(FileAttributes attributes, FileAttributes attribute_to_remove) {
+            return attributes & ~attribute_to_remove;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            prgExecution.Value = 0;
+            getFileNameLocation.ShowDialog();
+            this.file_location = getFileNameLocation.FileName;
+            this.path_segments = file_location.Split(Path.DirectorySeparatorChar);
+            txtFileName.Text = this.file_location;
+        }
+
+        private void btnDeSparce_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                prgExecution.Value = 0;
+                if (txtFileName.Text.Length > 0) {
+                    this.file_location = txtFileName.Text;
+                }
+                if (this.file_location.Length > 0)
+                {
+                    if (this.path_segments.Length <= 0){
+                        this.file_location = this.current_dir + txtFileName.Text;
+                        this.path_segments = file_location.Split(Path.DirectorySeparatorChar);
+                    }
+                    prgExecution.Value += 10;
+                    // Get file attributes/flags
+                    FileAttributes file_attrib = File.GetAttributes(this.file_location);
+                    // Remove read-only attributes
+                    if ((file_attrib & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                    {
+                        prgExecution.Value += 10;
+                        file_attrib = removeAttribute(file_attrib, FileAttributes.ReadOnly);
+                        File.SetAttributes(file_location, file_attrib);
+                        prgExecution.Value += 10;
+                    }
+                    else { 
+                        prgExecution.Value += 20; 
+                    }
+                    // Remove sparse flag
+                    if ((file_attrib & FileAttributes.SparseFile) == FileAttributes.SparseFile)
+                    {
+                        prgExecution.Value += 10;
+                        Process process = new Process();
+                        ProcessStartInfo start = new ProcessStartInfo();
+                        start.WindowStyle = ProcessWindowStyle.Hidden;
+                        start.FileName = "cmd.exe";
+                        start.Arguments = "/C fsutil sparse setflag \"" + file_location + "\" 0";
+                        process.StartInfo = start;
+                        bool status = process.Start();
+                        if (process.HasExited)
+                        {
+                            status = status & (process.ExitCode==0);
+                        }
+                        if(!status) throw new Exception("Could not remove sparse flag from file.");
+                        prgExecution.Value += 10;
+                    }
+                    else {
+                        prgExecution.Value += 20;
+                    }
+                    showInfo("Operation completed successfully!");
+                }
+                else {
+                    prgExecution.Value = 0;
+                    showError("There is no file selected. Please select a file and try again.");
+                }
+            }
+            catch (Exception exp) {
+                prgExecution.Value = 0;
+                showError(exp.Message);
+            }
+        }
+    }
+}
